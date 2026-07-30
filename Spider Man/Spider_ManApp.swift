2f2c2d1079cfdde18bc45042880f3b2cffc1cece
Peter Pan <apppeterpan@gmail.com>
2026-07-30 18:46:57 +0800

    Initial Commit

---
+++ Spider Man/Spider_ManApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spider_ManApp.swift
+//  Spider Man
+//
+//  Created by SHIH-YING PAN on 2026/7/30.
+//
+
+import SwiftUI
+
+@main
+struct Spider_ManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
